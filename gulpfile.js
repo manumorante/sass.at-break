243a36264d52256428e4b66ae30c998110a99576
Manu Morante <manuel.morante@betvictor.com>
2017-03-08 16:32:43 +0100

    Remove Compass and Bootstrap. Use Purecss. Build in the same branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,46 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
 var connect = require('gulp-connect');
-var compass = require('gulp-compass');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 var del = require('del');
-var deploy = require('gulp-gh-pages');
-var sassdoc = require('sassdoc');
 
 
-c = {
-  source: {
-    dir: 'source',
-    sass: 'source/sass'
-  },
-  build: {
-    dir: 'build',
-    css: 'build/css'
-  }
-};
-
-
-gulp.task('docs', function () {
-  var options = {
-    dest: c.build.dir +'/docs',
-    verbose: true
-  };
-
-  return gulp.src(c.source.sass +'/**/*.scss')
-    .pipe(sassdoc(options));
+gulp.task('scripts', ['clean'], function () {
+  return gulp.src('./source/javascripts/**/*.js')
+    .pipe(uglify())
+    .pipe(concat('application.js'))
+    .pipe(gulp.dest('./assets'));
 });
 
 
-gulp.task('html', function () {
-  gulp.src(c.source.dir + '/**/*.html').pipe(gulp.dest(c.build.dir));
-});
-
-
-gulp.task('styles', function () {
-  gulp.src(c.source.sass + '/*.scss')
-    .pipe(compass({
-      config_file: './compass.rb',
-      css: c.build.css,
-      sass: c.source.sass
-    }));
+gulp.task('sass', function () {
+  return gulp.src('./source/stylesheets/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets'))
 });
 
 
 gulp.task('watch', function () {
-  gulp.watch(c.source.sass + '/**/*.scss', ['styles']);
-  gulp.watch(c.source.dir + '/**/*.html', ['html']);
+  gulp.watch('./source/stylesheets/**/*.scss', ['sass']);
+  gulp.watch('./source/javascripts/**/*.js', ['scripts']);
 });
 
 
 gulp.task('server', function () {
-  connect.server({root: c.build.dir, port: 3000});
+  connect.server({root: './', port: 3000});
 });
 
 
 gulp.task('clean', function () {
-  return del([
-    c.build.dir,
-    '.publish',
-    '.sass-cache'
-  ])
-});
-
-
-gulp.task('build', ['html', 'styles']);
-
-
-gulp.task('deploy', function () {
-  return gulp.src(c.build.dir + '/**/*')
-    .pipe(deploy());
+  return del(['assets', '.sass-cache'])
 });
 
 
 gulp.task('default', ['clean'], function () {
-  gulp.start('build');
+  gulp.start('scripts');
+  gulp.start('sass');
   gulp.start('server');
   gulp.start('watch');
 });
